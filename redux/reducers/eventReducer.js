f46789235ae06bb94849c6b41b2b946607424cf2
KelvinMitaki <mitakikelvin1@gmail.com>
2020-08-25 00:01:22 +0300

    fixed data fetching from api and adding it to reduxstore

--- redux/reducers/eventReducer.js
+++ redux/reducers/eventReducer.js
@@ -1,9 +1,13 @@
+import { GET_MESSAGE } from "../actions/types";
+
 const INITIAL_STATE = {
   message: null
 };
 
 const eventReducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
+    case GET_MESSAGE:
+      return { ...state, message: action.payload };
     default:
       return state;
   }
