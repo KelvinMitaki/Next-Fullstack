21b2a168ea1ba0771ddd8e7589b9d09d2d60cdbb
KelvinMitaki <mitakikelvin1@gmail.com>
2020-08-25 21:26:06 +0300

    added event detailed info

--- pages/event/[id].js
+++ pages/event/[id].js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import EventDetailedHeader from "../../components/eventDetailedHeader/EventDetailedHeader";
 import Layout from "../../components/Layout";
 import { Grid } from "semantic-ui-react";
+import EventDetailedInfo from "../../components/eventDetailedInfo/EventDetailedInfo";
 
 export class event extends Component {
   render() {
@@ -11,6 +12,7 @@ export class event extends Component {
           <Grid>
             <Grid.Column width={10}>
               <EventDetailedHeader />
+              <EventDetailedInfo />
             </Grid.Column>
           </Grid>
         </div>
