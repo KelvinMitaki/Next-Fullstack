385a21fb60f8dc552de4d086eb9670b293dbca6a
KelvinMitaki <mitakikelvin1@gmail.com>
2020-08-26 17:42:38 +0300

    added an endpoint to update an event

--- server/controllers/event.js
+++ server/controllers/event.js
@@ -60,4 +60,65 @@ router.post(
     }
   }
 );
+
+router.post(
+  "/api/edit/event",
+  auth,
+  check("name").trim().not().isEmpty().withMessage("name must not be empty"),
+  check("about")
+    .trim()
+    .not()
+    .isEmpty()
+    .withMessage("about should not be empty"),
+  check("description")
+    .trim()
+    .isLength({ min: 20 })
+    .withMessage("please enter a description with 20 characters minimum"),
+  check("city").trim().not().isEmpty().withMessage("city must not be empty"),
+  check("venue")
+    .trim()
+    .not()
+    .isEmpty()
+    .withMessage("Venue field must not be empty"),
+  check("eventDate").isDate().withMessage("event date must be of date type"),
+  check("_id").not().isEmpty().withMessage("id must not be empty"),
+  async (req, res) => {
+    try {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(500).send({ message: errors.array()[0].msg });
+      }
+      const {
+        name,
+        about,
+        description,
+        city,
+        venue,
+        eventDate,
+        _id
+      } = req.body;
+      const userId = req.session.user._id;
+      const event = await Event.findById(_id);
+      if (!event) {
+        return res.status(404).send({ message: "No event with that id" });
+      }
+      if (event.host.toString() !== userId.toString()) {
+        return res
+          .status(401)
+          .send({ message: "Not authorised to edit this event" });
+      }
+      const updatedEvent = await Event.findByIdAndUpdate(_id, {
+        name,
+        about,
+        description,
+        city,
+        venue,
+        eventDate
+      });
+      res.send(updatedEvent);
+    } catch (error) {
+      res.status(500).send(error);
+    }
+  }
+);
 module.exports = router;
